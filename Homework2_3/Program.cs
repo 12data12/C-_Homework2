591f6041fb89d7ed14409fbf64583cb9dd56e457
Daria Dmitricheva <110775372+12data12@users.noreply.github.com>
2022-09-05 14:53:22 +0300

    Homework 2_3

---
+++ Homework2_3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день 
+
+Console.Write(" Enter the number of day of week ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void CheckingTheDayOfTheWeek (int dayNumber) {
+  if (dayNumber == 6 || dayNumber == 7) {
+  Console.WriteLine("It is a weekend! Huzzah!");
+  }
+  else if (dayNumber < 1 || dayNumber > 7) {
+    Console.WriteLine("Hmmm... There is no such day of the week");
+  }
+  else Console.WriteLine("Oh no... It is not a weekend :(");
+}
+
+CheckingTheDayOfTheWeek(dayNumber);
